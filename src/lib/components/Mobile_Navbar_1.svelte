d179c083ed1afa2e8ccc42c57bdb53cd6284f6e1
sisyphi <jordan.m.sibug@gmail.com>
2024-07-02 14:17:06 +0800

    feat: Reroutes pages to final routing

--- src/lib/components/Mobile_Navbar_1.svelte
+++ src/lib/components/Mobile_Navbar_1.svelte
@@ -26,11 +26,16 @@
 	use:trapFocus - Trap Focus within Menu
 	use:closeOnClickOutside & on:outclick - Close Menu on Outside Click
 -->
-<div use:trapFocus use:closeOnClickOutside on:outclick={closeMenu} class="absolute inset-0 z-50 h-screen px-6 py-2 overflow-scroll shadow-lg w-72 bg-neutral-800 md:px-8 md:py-4">
+<div
+	use:trapFocus
+	use:closeOnClickOutside
+	on:outclick={closeMenu}
+	class="absolute inset-0 z-50 h-screen px-6 py-2 overflow-scroll shadow-lg w-72 bg-neutral-800 md:px-8 md:py-4"
+>
 	<div class="flex justify-between w-auto">
-		<a href="/" class="self-center">
-			<span class="sr-only">Brand Name</span>
-			<img src="" alt="<BRAND NAME>" />
+		<a href="/timeblock-builder" class="self-center">
+			<span class="sr-only">Clock Block</span>
+			<img src="" alt="Clock Block" />
 		</a>
 
 		<button on:click={closeMenu} aria-expanded={isMenuOpen} class="p-2 hover:bg-neutral-700 hover:rounded-lg">
@@ -40,11 +45,13 @@
 	</div>
 
 	<div class="flex flex-col w-full py-4 text-left">
-		<a on:click={closeMenu} href="/" class="inline-flex items-center px-4 py-2 hover:underline"><span>Home</span></a>
-		<a on:click={closeMenu} href="/about" class="inline-flex items-center px-4 py-2 hover:underline"><span>About Us</span></a>
-		<a on:click={closeMenu} href="/sign-in" class="inline-flex items-center px-4 py-2 hover:underline"><span>Sign In</span></a>
-		<a on:click={closeMenu} href="/contact" class="inline-flex justify-center px-4 py-2 my-2 rounded-lg bg-neutral-200 hover:bg-neutral-100 text-neutral-800 hover:underline">
-			<span>Contact Us</span>
+		<a on:click={closeMenu} href="/about" class="inline-flex items-center px-4 py-2 hover:underline"><span>Learn</span></a>
+		<a
+			on:click={closeMenu}
+			href="/timeblock-builder"
+			class="inline-flex justify-center px-4 py-2 my-2 rounded-lg bg-neutral-200 hover:bg-neutral-100 text-neutral-800 hover:underline"
+		>
+			<span>Build</span>
 		</a>
 	</div>
 </div>
--- src/lib/components/Navbar_1.svelte
+++ src/lib/components/Navbar_1.svelte
@@ -20,9 +20,9 @@
 <header class="px-6 py-2 text-base font-bold text-center text-white bg-neutral-800 md:px-8 md:py-4">
 	<nav class="flex flex-wrap justify-between max-w-6xl mx-auto md:flex-nowrap">
 		<div class="flex justify-between w-full md:w-auto">
-			<a href="/" class="self-center">
-				<span class="sr-only">Brand Name</span>
-				<img src="" alt="<BRAND NAME>" />
+			<a href="/timeblock-builder" class="self-center">
+				<span class="sr-only">Clock Block</span>
+				<img src="" alt="Clock Block Logo" />
 			</a>
 
 			<button on:click={openMenu} aria-expanded={isMenuOpen} class="p-2 md:hidden hover:bg-neutral-700 hover:rounded-lg">
@@ -32,11 +32,13 @@
 		</div>
 
 		<div class="hidden md:py-0 md:flex md:flex-row md:w-auto">
-			<a on:click={closeMenu} href="/" class="inline-flex items-center px-4 py-2 hover:underline"><span>Home</span></a>
-			<a on:click={closeMenu} href="/about" class="inline-flex items-center px-4 py-2 hover:underline"><span>About Us</span></a>
-			<a on:click={closeMenu} href="/sign-in" class="inline-flex items-center px-4 py-2 hover:underline"><span>Sign In</span></a>
-			<a on:click={closeMenu} href="/contact" class="inline-flex justify-center px-4 py-2 rounded-lg bg-neutral-200 hover:bg-neutral-100 text-neutral-800 hover:underline">
-				<span>Contact Us</span>
+			<a on:click={closeMenu} href="/about" class="inline-flex items-center px-4 py-2 hover:underline"><span>Learn</span></a>
+			<a
+				on:click={closeMenu}
+				href="/timeblock-builder"
+				class="inline-flex justify-center px-4 py-2 rounded-lg bg-neutral-200 hover:bg-neutral-100 text-neutral-800 hover:underline"
+			>
+				<span>Build</span>
 			</a>
 		</div>
 	</nav>

3f822263089d66bcfc6c0a4f8c544f8095dd36e2
sisyphi <jordan.m.sibug@gmail.com>
2024-07-02 15:28:07 +0800

    feat: Add basic start page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,5 @@
 <script>
 	import '../app.css';
-
-	import Navbar_1 from '$lib/components/Navbar_1.svelte';
-	import MobileNavbar_1 from '$lib/components/Mobile_Navbar_1.svelte';
-
-	let isMenuOpen = false;
 </script>
-
-<Navbar_1 bind:isMenuOpen>
-	<MobileNavbar_1 slot="mobile-menu" bind:isMenuOpen></MobileNavbar_1>
-</Navbar_1>
 
 <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<section class="flex flex-col items-center content-center justify-center gap-4 pb-12 h-svh">
+	<h1 class="text-4xl italic font-bold text-center">What is your <br /><span class="underline">ideal day?</span></h1>
+	<p class="text-base">Show us your perfect schedule</p>
+	<a
+		href="/timeblock-builder"
+		class="flex flex-row items-center content-center justify-center px-6 py-3 text-lg font-bold text-center border-2 rounded-md text-neutral-800 border-neutral-800 hover:bg-neutral-800 hover:text-white"
+	>
+		Let's Start!
+	</a>
+</section>

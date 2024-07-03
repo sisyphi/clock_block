2fb617d049aef6c757ae13aa33af5f8dc7b0e171
sisyphi <jordan.m.sibug@gmail.com>
2024-07-03 16:37:01 +0800

    content: Add Learn Page and About Page

---
+++ src/routes/timeblock-builder/learn/+page.svelte
@@ -0,0 +1,30 @@
+<div class="px-6 md:px-8">
+	<div class="justify-around max-w-md mx-auto md:flex md:flex-col md:max-w-2xl md:gap-4">
+		<h1 class="sr-only">About</h1>
+		<section>
+			<h2 class="text-4xl font-bold">What is Time Blocking?</h2>
+
+			<p class="">
+				I think Todoist provides the simplest definition that suits what it is perfectly.
+				<br />
+				<span>"Time blocking is a time management method where you divide your day into blocks of time. Each block is dedicated to a specific task or group of tasks."</span>
+				<br />
+				It really depends on how granular you want to be. Personally, I love assigning very broad terms to my time blocks. It's cus' I'm not very good at following a schedule but
+				if you give me tasks I need to do in a chunk of time then they'll all be complete by the end.
+				<br />
+				I've seen others who go super deep with it too. Ali Abdaal would have time blocks down to the minute and I can only dream of having that much self-discipline.
+				<br />
+				It really depends on you to know how you want to use it, so let me just help you out with these examples.
+			</p>
+		</section>
+		<section>
+			<h2 class="text-4xl font-bold">How can I use it?</h2>
+			<ul>
+				<li>- Keeping track of your week days and week ends</li>
+				<li>- Planning an upcoming vacation trip</li>
+				<li>- Organizing your days to accommodate time for hobbies or passions</li>
+				<li>- Share your schedule on a story :)</li>
+			</ul>
+		</section>
+	</div>
+</div>
